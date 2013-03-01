7e08d834bfb871f83712347709bb67da967703d2
Jim Wang <turanuk@gmail.com>
2013-03-01 13:18:45 -0800

    Adding basic display in knockout.js

---
+++ SignalRGridDemo/App/EmployeeViewModel.js
@@ -0,0 +1,12 @@
+﻿var EmployeeViewModel = function () {
+  var self = this;
+  self.employees = ko.observableArray();
+
+  $.getJSON('api/employee', function (data) {
+    self.employees(data);
+  });
+}
+
+$(function () {
+  ko.applyBindings(new EmployeeViewModel());
+});
