4ccb6501735e0cf23d175888abd6b9cface9295d
Jim Wang <turanuk@gmail.com>
2013-03-01 15:37:42 -0800

    Adding connection clean-up code

--- SignalRGridDemo/Hubs/EmployeeHub.cs
+++ SignalRGridDemo/Hubs/EmployeeHub.cs
@@ -6,11 +6,19 @@ using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 using SignalRGridDemo.Models;
 using System.Data;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
 
 namespace SignalRGridDemo.Hubs {
   [HubName("employee")]
   public class EmployeeHub : Hub {
+    private static ConcurrentDictionary<string, List<int>> _mapping = new ConcurrentDictionary<string, List<int>>();
     private SignalRGridDemoContext db = new SignalRGridDemoContext();
+
+    public override Task OnConnected() {
+      _mapping.TryAdd(Context.ConnectionId, new List<int>());
+      return base.OnConnected();
+    }
 
     public void Lock(int id) {
       var employeeToPatch = db.Employees.Find(id);
@@ -18,6 +26,7 @@ namespace SignalRGridDemo.Hubs {
       db.Entry(employeeToPatch).State = EntityState.Modified;
       db.SaveChanges();
       Clients.Others.lockEmployee(id);
+      _mapping[Context.ConnectionId].Add(id);
     }
 
     public void Unlock(int id) {
@@ -26,6 +35,16 @@ namespace SignalRGridDemo.Hubs {
       db.Entry(employeeToPatch).State = EntityState.Modified;
       db.SaveChanges();
       Clients.Others.unlockEmployee(id);
+      _mapping[Context.ConnectionId].Remove(id);
+    }
+
+    public override Task OnDisconnected() {
+      foreach (var id in _mapping[Context.ConnectionId]) {
+        Unlock(id);
+      }
+      var list = new List<int>();
+      _mapping.TryRemove(Context.ConnectionId, out list);
+      return base.OnDisconnected();
     }
   }
 }
