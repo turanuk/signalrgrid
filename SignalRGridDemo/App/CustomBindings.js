8de6d47b0fc9319059e74df6c38dbae27f4bf67a
Jim Wang <turanuk@gmail.com>
2013-03-01 15:03:12 -0800

    Fixing custom bindings

--- SignalRGridDemo/App/CustomBindings.js
+++ SignalRGridDemo/App/CustomBindings.js
@@ -1,8 +1,8 @@
 ﻿ko.bindingHandlers.displayMoney = {
   init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-    $(element).html('$' + valueAccessor().toFixed(2));
+    $(element).html('$' + valueAccessor()().toFixed(2));
   },
   update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-    $(element).html('$' + valueAccessor().toFixed(2));
+    $(element).html('$' + valueAccessor()().toFixed(2));
   }
 };
