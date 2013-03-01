38c350ee69b2548a6a136df2c7e3c811551e95d0
Jim Wang <turanuk@gmail.com>
2013-03-01 13:47:42 -0800

    Adding custom bindings for money display

---
+++ SignalRGridDemo/App/CustomBindings.js
@@ -0,0 +1,8 @@
+﻿ko.bindingHandlers.displayMoney = {
+  init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+    $(element).html('$' + valueAccessor().toFixed(2));
+  },
+  update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+    $(element).html('$' + valueAccessor().toFixed(2));
+  }
+};
