c14707456b92e7a4caec8dbc9ba36aa7137ed479
Jim Wang <turanuk@gmail.com>
2013-03-01 14:07:45 -0800

    Updating view model to have observable property notifications

--- SignalRGridDemo/App/EmployeeViewModel.js
+++ SignalRGridDemo/App/EmployeeViewModel.js
@@ -3,8 +3,40 @@
   self.employees = ko.observableArray();
   self.loading = ko.observable(true);
 
+
+  self.watchModel = function (model, callback) {
+    for (var key in model) {
+      if (model.hasOwnProperty(key) && ko.isObservable(model[key])) {
+        self.subscribeToProperty(model, key, function (key, val) {
+          callback(model, key, val);
+        });
+      }
+    }
+  }
+
+  self.subscribeToProperty = function (model, key, callback) {
+    model[key].subscribe(function (val) {
+      callback(key, val);
+    });
+  }
+
+  self.modelChanged = function (model, key, val) {
+    alert(model.Id);
+    alert(key);
+    alert(val);
+  }
+
   $.getJSON('api/employee', function (data) {
-    self.employees(data);
+    self.employees(ko.utils.arrayMap(data, function (employee) {
+      var obsEmployee = {
+        Id: employee.Id,
+        Name: ko.observable(employee.Name),
+        Email: ko.observable(employee.Email),
+        Salary: ko.observable(employee.Salary),
+      }
+      self.watchModel(obsEmployee, self.modelChanged);
+      return obsEmployee;
+    }));
     self.loading(false);
   });
 }
