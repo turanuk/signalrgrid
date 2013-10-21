42d869625c4021204ee38ed1b70715e789f45916
Jim Wang (Microsoft) <jim.wang@microsoft.com>
2013-10-20 19:11:23 -0700

    Updating to SignalR 2.0

---
+++ SignalRGridDemo/App_Start/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+namespace SignalRGridDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
--- SignalRGridDemo/Global.asax.cs
+++ SignalRGridDemo/Global.asax.cs
@@ -15,7 +15,6 @@ namespace SignalRGridDemo {
     protected void Application_Start() {
       AreaRegistration.RegisterAllAreas();
 
-      RouteTable.Routes.MapHubs();
       WebApiConfig.Register(GlobalConfiguration.Configuration);
       FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
       RouteConfig.RegisterRoutes(RouteTable.Routes);
